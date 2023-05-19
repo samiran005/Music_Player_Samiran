c974b44f9508fe59cd48ce5cd864c984fe2613a9
samiarn845 <samiranmaity845@gmail.com>
2023-05-19 22:06:21 +0530

    modify some design and add new songs

--- musicData.js
+++ musicData.js
@@ -10,8 +10,18 @@ const musicData = [
         musicImageUrl: './Images/image2.jpg'
     },
     {
-        musicTitle: 'Paralyzed',
-        musicUrl: './music_libary/Paralyzed.mp3',
+        musicTitle: 'Crying In My Bedroom',
+        musicUrl: './music_libary/Crying-In-My-Bedroom.mp3',
         musicImageUrl: './Images/image3.jpg'
+    },
+    {
+        musicTitle: 'Dont Watch Me Cry',
+        musicUrl: './music_libary/Dont-Watch-Me-Cry.mp3',
+        musicImageUrl: './Images/image4.jpg'
+    },
+    {
+        musicTitle: 'Unity Alan Walker',
+        musicUrl: './music_libary/Unity-Alan-Walker.mp3',
+        musicImageUrl: './Images/image5.jpg'
     },
 ]
