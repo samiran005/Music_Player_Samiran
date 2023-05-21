fb8d27210e3ac7ba202eaa4e1ea6ea3d730528e0
samiarn845 <samiranmaity845@gmail.com>
2023-05-21 11:34:04 +0530

    add some new song

--- musicData.js
+++ musicData.js
@@ -1,21 +1,21 @@
 const musicData = [
     {
-        songTitle: 'Hardly In Love',
-        songUrl: './music_libary/Hardly-In-Love.mp3',
-        songImageUrl: './Images/image1.jpg',
-        songAuthor: 'Sam Tinnesz'
+        songTitle: 'Villain',
+        songUrl: './music_libary/VILLAIN.mp3',
+        songImageUrl: './Images/image1.png',
+        songAuthor: 'ft. Madison Beer'
     },
     {
-        songTitle: 'Control Zoe Wees',
-        songUrl: './music_libary/Control-Zoe-Wees.mp3',
-        songImageUrl: './Images/image2.jpg',
-        songAuthor: "Daddy's Eyes"
+        songTitle: 'Golden Hour',
+        songUrl: './music_libary/Golden-Hour.mp3',
+        songImageUrl: './Images/image2.png',
+        songAuthor: 'JVKE'
     },
     {
-        songTitle: 'Crying In My Bedroom',
-        songUrl: './music_libary/Crying-In-My-Bedroom.mp3',
-        songImageUrl: './Images/image3.jpg',
-        songAuthor: 'Chloe Adams'
+        songTitle: 'Inside',
+        songUrl: './music_libary/Inside.mp3',
+        songImageUrl: './Images/image3.png',
+        songAuthor: 'Netflix'
     },
     {
         songTitle: 'Dont Watch Me Cry',
@@ -28,5 +28,23 @@ const musicData = [
         songUrl: './music_libary/Unity-Alan-Walker.mp3',
         songImageUrl: './Images/image5.jpg',
         songAuthor: 'Alan Walker'
+    },
+    {
+        songTitle: 'Hardly In Love',
+        songUrl: './music_libary/Hardly-In-Love.mp3',
+        songImageUrl: './Images/image6.jpg',
+        songAuthor: 'Sam Tinnesz'
+    },
+    {
+        songTitle: 'Control Zoe Wees',
+        songUrl: './music_libary/Control-Zoe-Wees.mp3',
+        songImageUrl: './Images/image7.jpg',
+        songAuthor: "Daddy's Eyes"
+    },
+    {
+        songTitle: 'Crying In My Bedroom',
+        songUrl: './music_libary/Crying-In-My-Bedroom.mp3',
+        songImageUrl: './Images/image8.jpg',
+        songAuthor: 'Chloe Adams'
     },
 ]
